c78815cb92c8f5ac6867222aa0d22384e0f24507
Michael Uloth <hello@michaeluloth.com>
2018-08-29 23:00:23 -0400

    Remove service worker for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-postcss',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-offline',
+    'gatsby-plugin-postcss', // commenting this out restores CSS minification
   ],
 }
